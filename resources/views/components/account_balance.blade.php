c8b19f435bf99d2e739b8853d43bea4ff90fadd2
Travier Moorlag <moorlagt@gmail.com>
2020-11-01 19:55:24 -0500

    Small fix for account balance component

--- resources/views/components/account_balance.blade.php
+++ resources/views/components/account_balance.blade.php
@@ -1,4 +1,5 @@
 @php
 $textClass = ($currentAccountBalance >= 1 ? 'text-success' : 'text-danger')
+
 @endphp
-<h5>Current Account Balance: <span class="{{ $textClass }}">${{ $currentAccountBalance }}</span></h5>
+<h5>Current Account Balance: <span class="{{ ($currentAccountBalance == 0 ? '' : $textClass) }}">${{ $currentAccountBalance }}</span></h5>
